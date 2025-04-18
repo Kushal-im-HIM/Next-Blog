a0bcf54f9dcfeb8f539b1815ad8a9f2b5d39fab8
Kushal <kushalpothumanchi@gmail.com>
2025-04-18 08:26:47 +0530

    fix: mark write page as client component

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import styles from "./blogPage.module.css"
 import CardList from '@/components/cardList/CardList'
--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import styles from "./singlePage.module.css"
 import Menu from '@/components/Menu/Menu'
