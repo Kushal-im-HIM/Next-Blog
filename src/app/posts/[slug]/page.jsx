a4d5c8f62d128f72e77a054d722ae27a751c49aa
Kushal <kushalpothumanchi@gmail.com>
2025-04-18 15:03:51 +0530

    solved client side errors

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,4 +1,3 @@
-"use client"
 import React from 'react'
 import styles from "./singlePage.module.css"
 import Menu from '@/components/Menu/Menu'
--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -31,7 +31,7 @@ const Comments = ({ postSlug }) => {
   const [desc, setDesc] = useState("");
 
   const handleSubmit = async () => {
-    await fetch("/api/comments", {
+    await fetch(`${process.env.NEXTAUTH_URL}/api/comments`, {
       method: "POST",
       body: JSON.stringify({ desc, postSlug }),
     });
